7ca7f74ff145259bea936e8d4b26e860fb9af11d
markusm1406-beep <markusm1406@gmail.com>
2026-08-02 20:28:12 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,8 @@
-const CACHE_NAME = 'lm-rechner-v3';
+const CACHE_NAME = 'lm-rechner-v4';
 const ASSETS = [
   'index.html',
-  'manifest.json'
+  'manifest.json',
+  'icon.png'
 ];
 
 self.addEventListener('install', (event) => {
